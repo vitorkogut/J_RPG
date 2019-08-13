095b319f52e4020460e100476d1184ce944ad33f
João Vitor Specht Kogut <49065277+vitorkogut@users.noreply.github.com>
2019-08-13 11:12:49 -0300

    Add files via upload

---
+++ src/combate/main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package combate;
+
+/**
+ *
+ * @author 6462340
+ */
+
+public class main {
+public static player Jogador = new player(); // SETA JOGADOR COMO GLOBAL
+   
+    public static void main(String[] args){
+  
+    inimigo Inimigo = new inimigo();
+    Jogador.setPontosUP(10);
+    
+    frameMain MainFrame = new frameMain();
+    MainFrame.setVisible(true);
+        
+        
+    }
+ 
+}
---
+++ src/combate/player.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package combate;
+
+/**
+ *
+ * @author 6462340
+ */
+public class player {
+
+    /**
+     * @return the pontosUP
+     */
+    public int getPontosUP() {
+        return pontosUP;
+    }
+
+    /**
+     * @param pontosUP the pontosUP to set
+     */
+    public void setPontosUP(int pontosUP) {
+        this.pontosUP = pontosUP;
+    }
+
+    /**
+     * @return the forca
+     */
+    public int getForca() {
+        return forca;
+    }
+
+    /**
+     * @param forca the forca to set
+     */
+    public void setForca(int forca) {
+        this.forca = forca;
+    }
+
+    /**
+     * @return the defesa
+     */
+    public int getDefesa() {
+        return defesa;
+    }
+
+    /**
+     * @param defesa the defesa to set
+     */
+    public void setDefesa(int defesa) {
+        this.defesa = defesa;
+    }
+
+    /**
+     * @return the vida
+     */
+    public int getVida() {
+        return vida;
+    }
+
+    /**
+     * @param vida the vida to set
+     */
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    
+    
+    int pontosUP = 0;
+    private int forca =0;
+    private int defesa = 0;
+    private int vida = 0;
+    
+    
+    
+}
